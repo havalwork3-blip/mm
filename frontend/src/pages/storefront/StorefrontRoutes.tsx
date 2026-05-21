ad393de4cf71eb5c1f48fe74443a22e1d4e7db49
haval <haval@gmail.com>
2026-05-21 03:18:56 +0300

    online shopping

--- frontend/src/pages/storefront/StorefrontRoutes.tsx
+++ frontend/src/pages/storefront/StorefrontRoutes.tsx
@@ -3,7 +3,6 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import './storefront.css'
 import { isStorefrontMode } from '../../lib/storefrontConfig'
 import { StorefrontCatalog } from './StorefrontCatalog'
-import { StorefrontCatalogProvider } from './storefrontCatalogContext'
 import {
   StorefrontAboutPage,
   StorefrontContactPage,
